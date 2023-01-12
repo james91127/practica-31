926fd3938fe44d70963e1039ac33892d4866bbb4
james91127 <james200327@gmail.com>
2023-01-12 11:03:51 +0100

    cambio

--- practica 31/src/prueba.java
+++ practica 31/src/prueba.java
@@ -3,7 +3,7 @@ public class prueba {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		System.out.println("esto es un cambio");
+		System.out.println("esto es un cambi");
 	}
 
 }
